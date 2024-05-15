30096ca163eaf2c0486dbb8da1766f10b29a4791
Azlan-saja <azlansaja19@gmail.com>
2024-05-15 18:31:31 +0700

    Tombol Kurang

--- Aplikasi Variabel Tipe Data dan Operator/Form1.vb
+++ Aplikasi Variabel Tipe Data dan Operator/Form1.vb
@@ -16,4 +16,21 @@
         MsgBox(Pesan, MsgBoxStyle.Information, "Hasil Tambah")
     End Sub
 
+    Private Sub B_Kurang_Click(sender As Object, e As EventArgs) Handles B_Kurang.Click
+
+        Dim var_nilai_1 As Integer
+        var_nilai_1 = Tb_Nilai1.Text
+
+        Dim var_nilai_2 As Integer
+        var_nilai_2 = Tb_Nilai2.Text
+
+        Dim var_hasil As Integer
+        var_hasil = var_nilai_1 - var_nilai_2
+
+        Dim pesan As String
+        pesan = var_nilai_1 & " - " & var_nilai_2 & " = " & var_hasil
+        MsgBox(pesan, , "Hasil Kurang")
+
+    End Sub
+
 End Class
